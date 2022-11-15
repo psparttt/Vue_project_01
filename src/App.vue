7945ea2e32df4b95c9465a5100a631f946046b27
psparttt <pongsapus.it@gmail.com>
2022-11-15 17:08:56 +0700

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <img src="logo.png" />
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link active">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link active">About</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
   <router-view/>
 </template>
 
@@ -28,3 +50,4 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
+  
   <div class="home">
+    <h1>This is an Home page</h1>
     
   </div>
 </template>
