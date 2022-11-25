93831ac587149a89bfab86a3edf2fe6dd49ca69e
psparttt <pongsapus.it@gmail.com>
2022-11-25 17:40:54 +0700

    new commit

--- src/components/WorkExperian.vue
+++ src/components/WorkExperian.vue
@@ -1,17 +1,21 @@
-<template>
+<template >
   <div class="background-area">
+    <font-awesome-icon icon="fa-regular fa-star" />
     <h1 class="text-warning text-bold text-padding">About Me</h1>
-    <h6 class="text-white text-bold">I always like to learn new things.The skills that are available</h6>
+    <h6 class="text-white text-bold">I always like to learn new things.The skills that are available </h6>
     <h6 class="text-white text-bold">now are Getting the job done in a timely manner and i can solve</h6>
     <h6 class="text-white text-bold">immediate problems as well.</h6>
-    <h6 class="text-warning text-bold mt-20">My Skill</h6>
+    <h6 class="text-warning text-bold mt-20">What I'm good at</h6>
     <div class="box-flex">
-      <div class="bg-box">
-        <h2>test</h2>
+        <div class="box-flex bg-box">
+            <i class="fa-brands fa-html5 text-white icon-size"></i>
+           <i class="fa-brands fa-js text-white icon-size ml-5"></i>
+           <i class="fa-brands fa-css3-alt text-white icon-size ml-5 "></i>
+           <i class="fa-brands fa-python text-white icon-size ml-5"></i>
+           <i class="fa-brands fa-vuejs text-white icon-size ml-5"></i>
+        
       </div>
-      <div class="bg-box"></div>
-      <div class="bg-box"></div>
-      <div class="bg-box"></div>
+      
     </div>
   </div>
 </template>
@@ -42,8 +46,8 @@ export default {
 .bg-box {
   border-radius: 10px;
   margin-left: 40px;
-  width: 200px;
-  height: 200px;
+  width: 400px;
+  height: 100px;
   background: #212121;
 }
 .box-flex {
@@ -54,5 +58,15 @@ export default {
 }
 .mt-20{
   margin-top: 20px;
+}
+.ml-5{
+  margin-left: 5px;
+}
+.icon-size{
+  width: 50px;
+  height: 50px;
+}
+.pd-top-5{
+  padding-top: 5px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, far, fab)
+dom.watch();
+
 const app= createApp({
     data() {
         return {
@@ -11,6 +20,8 @@ const app= createApp({
         }
     },
 })
-
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+.use(store)
+.use(router)
+.component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app')
